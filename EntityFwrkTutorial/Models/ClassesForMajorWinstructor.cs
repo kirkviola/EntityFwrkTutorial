067815d0796199793e4916b11b6a5ccffd55ca67
Matthew Kirkendall <kirk.viola@gmail.com>
2021-10-06 08:56:20 -0400

    Add project files.

---
+++ EntityFwrkTutorial/Models/ClassesForMajorWinstructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityFwrkTutorial.Models
+{
+    public partial class ClassesForMajorWinstructor
+    {
+        public string MajorCode { get; set; }
+        public string Major { get; set; }
+        public string Class { get; set; }
+        public string Instructor { get; set; }
+    }
+}
---
+++ EntityFwrkTutorial/Program.cs
@@ -0,0 +1,51 @@
+﻿using EntityFwrkTutorial.Models;
+using System;
+using System.Linq;
+
+namespace EntityFwrkTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new EdDbContext();
+            var majors = _context.Majors.ToList();
+            var students = _context.Students.ToList();
+            var classes = _context.Classes.ToList();
+            var majorsclasses = _context.MajorClasses.ToList();
+
+            var gpaView = students.Where(s => s.Gpa > students.Average(s => s.Gpa)).OrderBy(s => s.Lastname);
+
+            foreach(var v in gpaView)
+                Console.WriteLine($"{v.Lastname}, {v.Firstname}: GPA is {v.Gpa}");
+            Console.WriteLine("---");
+
+            var MajorsStudents = from m in majors
+                                join s in students
+                                on m.Id equals s.MajorId
+                                where m.Code == "MATH"
+                                orderby s.Lastname
+                                select new{ m, s};
+
+            foreach(var ms in MajorsStudents)
+                Console.WriteLine($"{ms.s.Firstname} {ms.s.Lastname} | {ms.m.Code} | {ms.m.Description}");
+            Console.WriteLine("---");
+
+            var StudentsClasses = from s in students
+                                  join m in majors
+                                  on s.MajorId equals m.Id
+                                  join mc in majorsclasses
+                                  on m.Id equals mc.MajorId
+                                  join c in classes
+                                  on mc.ClassId equals c.Id
+                                  where m.Description == "Engineering"
+                                  orderby s.Lastname descending
+                                  select new { s, m, mc, c };
+
+            foreach(var smmcc in StudentsClasses)
+            {
+                Console.WriteLine($"{smmcc.s.Lastname} | {smmcc.m.Description} | {smmcc.c.Code}");
+            }
+        }
+    }
+}
